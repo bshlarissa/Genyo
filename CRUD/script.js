3d2497b8d8fa0563a85eab805b4cb87bff87e605
bshlarissa <bslarissa1112@gmail.com>
2023-05-20 12:08:25 -0300

    Começando o JS

---
+++ CRUD/script.js
@@ -0,0 +1,14 @@
+let dados = [
+    {id:1, nome: "larissa", idade:"21 anos"},
+    {id:1, nome: "larissa", idade:"21 anos"},
+]
+
+function leiaTodos(){
+    localStorage.setItem("object", JSON.stringify(dados));
+     var dadosTabela = document.querySelector (".tabelaDados");
+
+     var object = localStorage.getItem('object');
+     var objectdata = JSON.parse (object);
+     var elements = "";
+
+    }   
