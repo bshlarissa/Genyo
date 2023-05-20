cdc71ef4ef988e6557d72c9cbcabf91c368c2f0a
bshlarissa <bslarissa1112@gmail.com>
2023-05-20 16:31:57 -0300

    Update agora o codigo recebe os valores inseridos.

--- CRUD/script.js
+++ CRUD/script.js
@@ -1,15 +1,46 @@
 let dados = [
-    {id:1, nome: "larissa", idade:"21 anos"},
-    {id:1, nome: "larissa", idade:"21 anos"},
+    {id: 1, nome: "larissa", idade: "21 anos"},
+    {id: 2, nome: "pedro", idade: "22 anos"}
 ]
 
 function leiaTodos(){
     localStorage.setItem("object", JSON.stringify(dados));
-     var dadosTabela = document.querySelector (".tabelaDados");
+     var tabelaDados = document.querySelector (".dadosTabela");
 
      var object = localStorage.getItem('object');
-     var objectdata = JSON.parse (object);
+     var objectdata = JSON.parse(object);
      var elements = "";
 
-     
+    objectdata.map(record => (
+        elements += `<tr>
+            <td>${record.nome}</td>
+            <td>${record.idade}</td>
+            <td>
+            <button>Editar</button>
+            <button>Apagar</button>
+        </td>
+        </tr>`
+    ))
+
+    tabelaDados.innerHTML = elements;
     }   
+
+    function create() {
+        document.querySelector(".criarFormulario").style.display = "block"; 
+        document.querySelector(".adicionarFormulario").style.display = "none ";
+    }
+
+    function add() {
+        var nome = document.querySelector(".nome").value;
+        var idade = document.querySelector(".idade").value;
+
+        var newObject = {id: 3, nome: nome, idade: idade};
+        dados.push(newObject);
+        
+
+        document.querySelector(".criarFormulario").style.display = "none"; 
+        document.querySelector(".adicionarFormulario").style.display = "block"; 
+
+        leiaTodos(); 
+
+    }
